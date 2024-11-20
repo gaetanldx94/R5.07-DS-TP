1979f71fbaa6ca62758f62b175cb7d23032020a8
Ledoux Gaétan <41428973+gaetanldx94@users.noreply.github.com>
2024-11-20 14:26:48 +0100

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,19 @@
+public class Q6 {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Veuillez fournir un nombre en paramètre.");
+            return;
+        }
+        try {
+            int n = Integer.parseInt(args[0]);
+            for (int i = n; i > 0; i--) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("x");
+                }
+                System.out.println();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Le paramètre doit être un entier.");
+        }
+    }
+}
