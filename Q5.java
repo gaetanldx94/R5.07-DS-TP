0d5870bb4bd08a6ee7bc24fa261131a853d53048
Ledoux Gaétan <41428973+gaetanldx94@users.noreply.github.com>
2024-11-20 14:25:29 +0100

    afficher des ‘x’ à la place des ‘*’

--- Q5.java
+++ Q5.java
@@ -8,7 +8,7 @@ public class Q5 {
             int n = Integer.parseInt(args[0]);
             for (int i = n; i > 0; i--) {
                 for (int j = 0; j < i; j++) {
-                    System.out.print("*");
+                    System.out.print("x");
                 }
                 System.out.println();
             }
